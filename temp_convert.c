d9d147a0b697ff81fb313a136e1195c8d7db35aa
Naman Manchanda <35381035+namanmanchanda09@users.noreply.github.com>
2018-02-05 10:01:04 +0530

    Add files via upload

---
+++ temp_convert.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+
+    float c,f;
+    scanf("%f",&c);
+    f = (((float)9/(float)5)*c)+(float)32;
+    printf("%f" , f);
+    getch();
+    return 0;
+}
