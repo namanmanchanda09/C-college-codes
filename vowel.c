4bd9c860cc90fa2f80550dbcf47268eaecff2b40
Naman Manchanda <35381035+namanmanchanda09@users.noreply.github.com>
2018-02-05 10:11:39 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    char a;
+    scanf("%c" , &a);
+    if (a=='a')
+    {
+        printf("vowel");
+    }
+    else if (a=='e')
+    {
+        printf("vowel");
+    }
+    else if (a=='i')
+    {
+        printf("vowel");
+    }
+    else if (a=='o')
+    {
+        printf("vowel");
+    }
+    else if (a=='u')
+    {
+        printf("vowel");
+    }
+    else
+    {
+        printf("Not a vowel");
+    }
+    getch();
+    return 0;
+
+}
